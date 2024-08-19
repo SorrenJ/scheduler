8517a6febf20c6f2110fe5194b1102ec455f85d0
SorrenJ <soalexjao@gmail.com>
2024-08-18 18:40:14 -0700

    install prop-types to validate interviewer props to check if it is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from 'prop-types'; 
 
 import "components/InterviewerList.scss";
 
 import InterviewerListItem from "components/InterviewerListItem";
 
 function InterviewerList(props) {
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewers  = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -17,6 +18,13 @@ function InterviewerList(props) {
       />
     );
   });
+
+  InterviewerList.propTypes = {
+
+    // Add .toString() function to the end of the variable name to easily turn the array into a string
+    interviewers: PropTypes.array.isRequired.toString()
+  };
+  
 
   return (
     <section className="interviewers">
